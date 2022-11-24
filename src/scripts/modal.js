49634bc799139d96212120229e701279765fe263
Vinicius <viniciusribeiro.dev@hotmail.com>
2022-11-24 20:46:58 -0300

    entrega S1-14

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,35 @@
-/* Desenvolva seu código aqui... */
+const modal = document.querySelector(".modal__full");
+
+const openModal = document.querySelector(".forgot__password");
+
+const closeModal = document.querySelector(".close__modal");
+
+const closeModalButton = document.querySelector(".close__modal--button");
+
+const codigo = document.querySelector("#codigo");
+
+function openModal_() {
+  openModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "flex";
+  });
+}
+
+openModal_();
+
+function closeModal_() {
+  closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "none";
+  });
+  if (codigo.value != undefined) {
+
+    closeModalButton.addEventListener("click", (e) => {
+        console.log(codigo.value);
+      e.preventDefault();
+      modal.style.display = "none";
+    });
+  }
+}
+
+closeModal_();
